741bbf91da64473d00e53947cc6203a8ac3b81f0
Krzysztof Antoniak <krzysztof@antoniak.in>
2018-03-03 19:10:07 +0100

    Implement gRPC service in :api-server

--- api-server/src/main/java/com/kantoniak/examplegrpc/server/ApiServer.java
+++ api-server/src/main/java/com/kantoniak/examplegrpc/server/ApiServer.java
@@ -1,28 +1,67 @@
 package com.kantoniak.examplegrpc.server;
 
 import com.kantoniak.examplegrpc.proto.Entry;
+import com.kantoniak.examplegrpc.proto.EntryServiceGrpc;
+import com.kantoniak.examplegrpc.proto.GetAllRequest;
+import com.kantoniak.examplegrpc.proto.GetAllResponse;
+
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 
 class ApiServer {
+    private final static int PORT = 8800;
+    private Server grpcServer;
+
+    private void start() throws IOException {
+        grpcServer = ServerBuilder.forPort(PORT)
+                .addService(new EntryServiceImpl())
+                .build()
+                .start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(ApiServer.this::stop));
+    }
+
+    private void stop() {
+        if (grpcServer == null) {
+            System.out.println("Server shutting down...");
+            grpcServer.shutdown();
+            System.out.println("Server shut down.");
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (grpcServer != null) {
+            grpcServer.awaitTermination();
+        }
+    }
+
     public static void main(String[] args) {
 
-        final ApiServer authServer = new ApiServer();
+        final ApiServer apiServer = new ApiServer();
         try {
-            System.out.println("Starting API Server...");
-            authServer.playWithProtos();
-        } catch (Exception e) {
+            System.out.println("Starting server...");
+            apiServer.start();
+            System.out.println(String.format("The server is now running on port %s", PORT));
+            apiServer.blockUntilShutdown();
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void playWithProtos() {
-        Entry entry = Entry.newBuilder()
-                .setId(1)
-                .setName("Test entry 1")
-                .build();
+    static class EntryServiceImpl extends EntryServiceGrpc.EntryServiceImplBase {
 
-        String message = "It worked! Entry name is: " + entry.getName();
-
-        System.out.println("ApiServer.main() called.");
-        System.out.println(message);
+        @Override
+        public void getAll(GetAllRequest request, StreamObserver<GetAllResponse> responseObserver) {
+            GetAllResponse response = GetAllResponse.newBuilder()
+                    .addEntries(Entry.newBuilder().setId(1).setName("Entry 1"))
+                    .addEntries(Entry.newBuilder().setId(2).setName("Entry 2"))
+                    .addEntries(Entry.newBuilder().setId(3).setName("Entry 3"))
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
 }
